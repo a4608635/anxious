0ea960401bd54b6d27f71e6aa17c9d46e0f28952
a4608635 <57396749+a4608635@users.noreply.github.com>
2020-07-07 20:29:53 +0800

    451515145

---
+++ anxious/Assets/Scenes/NPC.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public float speed = 0;
+
+    public Animator ani;
+    public Transform tra;
+
+    public void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    public void Move()
+    {
+        
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        
+        transform.Translate(speed * Time.deltaTime * h, 0, Time.deltaTime * v);
+        
+    }
+
+    public void unit()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ani.SetTrigger("JOMP");
+        }
+    }
+
+
+    private void Update()
+    {
+        Move();
+
+        unit();
+    }
+}
